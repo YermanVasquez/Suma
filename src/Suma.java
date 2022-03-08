443932f89431043667a7228f6f5ab19d8daf5986
German Vasquez Santiago <you@example.com>
2022-03-08 13:45:12 -0600

    Tarea de paradigma de programación suman

--- src/Suma.java
+++ src/Suma.java
@@ -1,4 +1,21 @@
-
+// Programa que recibe dos números y muestra la suma.
+import java.util.Scanner; // el progrma usa la clase Scanner
 public class Suma {
-	private int a;
-}
+	// El metodo main empieza la ejecución de la aplicación en java
+	public static void main (String[]args)
+	{
+		Scanner entrada = new Scanner(System.in);
+		int numero1;
+		int numero2;
+		int suma;
+		System.out.println("Ingresa el primer dato: ");
+		numero1 = entrada.nextInt();
+		System.out.println("Ingresa el segundo número: ");
+		numero2 = entrada.nextInt();
+		suma = numero1 + numero2;
+		System.out.println("El resultado es: "+suma);
+	}
+	}
+	
+	
+
